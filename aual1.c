33bb42bffef0a59d96705d733f7ea5f4017c2d60
Igor Christofidis de Lima <igor.christofidis@dinfor.uniceub>
2025-02-26 09:42:41 -0300

    Aula 1

---
+++ aual1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int A, B, S;
+    printf("escreva um numero: ");
+scanf("%d", &A);
+    printf("escreva outro numero: ");
+scanf("%i", &B);
+S = A + B;
+    printf("a soma dos seus numeros e: ");
+    printf("%d + %i = %d", A, B, S);
+    return 0; 
+
+}
